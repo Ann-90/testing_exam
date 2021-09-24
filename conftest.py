32a43eec04285055e40c287b0d70b62e6b5662ee
Ann Vorobyeva <ann@fastlaw.io>
2021-09-24 23:59:41 +0300

    env

--- conftest.py
+++ conftest.py
@@ -1,11 +1,27 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    lang = request.config.getoption("language")
+    opt = Options()
+    opt.add_experimental_option('prefs', {'intl.accept_languages': lang})
+
+    if lang:
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=opt)
+    else:
+        raise pytest.UsageError("chose the --language")
+
     yield browser
+    time.sleep(3)
     print("\nquit browser..")
     browser.quit()
