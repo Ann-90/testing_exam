f66aee27ce239c682178feb4a7939f7ea300c27b
Ann Vorobyeva <ann@fastlaw.io>
2021-09-26 01:37:01 +0300

    review

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ links = list(map(
 link_invalid = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -16,6 +17,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
 
 
+@pytest.mark.need_review
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link_invalid)
@@ -25,12 +27,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link_invalid)
     page.open()
     page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link_invalid)
